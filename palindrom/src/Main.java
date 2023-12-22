16d6101341770df67d0c5d1b83f5abe05367b9d4
dogusskarakus <doguss.karakus@gmail.com>
2023-12-22 18:06:53 +0300

    Polidrom

--- palindrom/src/Main.java
+++ palindrom/src/Main.java
@@ -1,9 +1,26 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("merhaba");
-        System.out.println("ksfb");
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
 
         }
+        if (number == reverseNumber)
+            return true;
+
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(101));
+
+        }
+
 }
